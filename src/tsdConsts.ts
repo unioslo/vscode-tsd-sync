02c4763b7af05773b3ce883df5ab120b7a71ddf0
Florian Krull <flo.krull@gmail.com>
2023-05-02 14:41:17 +0200

    fix import link example and regex

--- src/tsdConsts.ts
+++ src/tsdConsts.ts
@@ -4,10 +4,10 @@ const exampleUuid = randomUUID();
 
 export namespace tsdConsts {
   export const importUrlRegEx = RegExp(
-    /^https:\/\/data\.tsd\.usit\.no\/i\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
+    /^https:\/\/data\.tsd\.usit\.no\/[0-9a-z]\/[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/
   );
   export const importLink = (linkId: UUID): string =>
-    `https://data.tsd.usit.no/i/${linkId}`;
+    `https://data.tsd.usit.no/c/${linkId}`;
   export const importLinkExample: string = importLink(exampleUuid);
   export const importLinkPlaceholder: string = importLink(
     "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"
